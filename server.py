bef7fa107f8dcb0ca164c65e663dc8b1eabe1ca6
zxbuzzy <zxbuzzy@gmail.com>
2021-07-15 23:03:47 +0300

    Добавлен JS код

--- server.py
+++ server.py
@@ -12,16 +12,17 @@ app = FastAPI()
 
 #Сгенерировано в openssl
 SECRET_KEY = 'd485c8826e9aaf9a025957ba0a71347736891c8a319e5bcc6b0d0f85384d68df'
+PASSWORD_SALT = '9a616eb54605e906551accca1102af272e9bea1ac6ac2caaaba1b1c47085a378'
 
 users = {
     'nikita@user.com': {
         'name': 'Никита',
-        'password': 'password1',
+        'password': '16a5e732974911b9ba427d02a344f3d3627aecae25edefce18b048b83ad5c76b',
         'balance': 100_000
     },
     'alice@user.com': {
         'name': 'Алиса',
-        'password': 'password2',
+        'password': 'd0c4daaea67fa83d80a862e1134c3b674d46767bb998fe2f19bcb368afb6dd07',
         'balance': 500_000
     }
 }
@@ -44,6 +45,11 @@ def get_username_from_signed_string(username_signed: str) -> Optional[str]:
     if hmac.compare_digest(valid_sign, sign):
         return username
 
+
+def verify_password(username: str, password: str) -> bool:
+    password_hash = hashlib.sha256((password + PASSWORD_SALT).encode()).hexdigest().lower()
+    stored_password_hash = users[username]["password"].lower()
+    return password_hash == stored_password_hash
 
 # Декоратор для указания адреса и типа запроса на который нужно вызвать функцию
 @app.get('/')
@@ -69,20 +75,18 @@ def index_page(username: Optional[str] = Cookie(default=None)):
 
 @app.post('/login')
 def process_login_page(username: str = Form(...), password: str = Form(...)):
-    try:
-        user = users[username]
-        if user['password'] != password:
-            return Response('Неверный логин', media_type='text/html')
-        else:
-            response = Response(
-                f'Привет, {user["name"]}<br />Баланс: {user["balance"]}',
-                media_type='text/html'
-            )
-            username_signed = base64.b64encode(username.encode()).decode() + '.' + \
-                sign_data(username)
-            response.set_cookie(key='username', value=username_signed)
-            return response
-    except KeyError:
-        return Response("Я вас не знаю:(", media_type='text/html')
+    user = users.get(username)
+    print(user, password)
+    if not user or not verify_password(username, password):
+        return Response("Я вас не знаю", media_type='text/html')
+    
+    response = Response(
+        f"Привет, {user['name']}!<br />Баланс: {user['balance']}",
+        media_type='text/html')
+    username_signed = base64.b64encode(username.encode()).decode() + "." + sign_data(username)
+    response.set_cookie(key='username', value=username_signed)
+    
+    return response
+    
 
 
